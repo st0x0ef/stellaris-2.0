3df2fcb760d0c38dcf71a0c41e6ad6d446b95d7f
TATHAN <costa.tathan@gmail.com>
2025-07-24 18:49:17 +0200

    Add logger

--- common/src/main/java/org/exodusstudio/stellaris/Stellaris.java
+++ common/src/main/java/org/exodusstudio/stellaris/Stellaris.java
@@ -1,7 +1,11 @@
 package org.exodusstudio.stellaris;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Stellaris {
     public static final String MOD_ID = "stellaris";
+    public static final Logger LOG = LoggerFactory.getLogger("Stellaris");
 
     public static void init() {
 
