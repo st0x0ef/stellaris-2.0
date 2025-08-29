f2c040802a91e77792403561dfc29c64741742c6
eirmax <eirmax8@gmail.com>
2025-08-29 16:49:44 +0700

    Forge tags

---
+++ neoforge/src/main/java/org/exodusstudio/stellaris/neoforge/datagen/tags/ForgeItemTagsProvider.java
@@ -0,0 +1,30 @@
+package org.exodusstudio.stellaris.neoforge.datagen.tags;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.ItemTags;
+import net.neoforged.neoforge.common.data.ItemTagsProvider;
+import org.exodusstudio.stellaris.common.registries.ItemsRegistry;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ForgeItemTagsProvider extends ItemTagsProvider {
+
+
+    public ForgeItemTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, String modId) {
+        super(output, lookupProvider, modId);
+    }
+
+    @Override
+        protected void addTags(HolderLookup.Provider provider) {
+        // example
+            tag(ItemTags.RAILS).add(
+                    ItemsRegistry.DESH_INGOT.get()
+            );
+
+            // custom
+            // tag(ModItemTags.YOUR_CUSTOM_TAG).add(
+            //     ItemsRegistry.YOUR_ITEM.get()
+            // );
+        }
+    }
