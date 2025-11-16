914cff8219482a28277f94e1d48e6cdf6180e1d2
Ilyuxadwa <ilyuxadwa.work@gmail.com>
2025-11-16 22:04:01 +0200

    Moon iron ore + stoned wood

--- common/src/main/java/org/exodusstudio/stellaris/common/registries/BlocksRegistry.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/registries/BlocksRegistry.java
@@ -33,6 +33,9 @@ public final class BlocksRegistry {
      */
     public static final BlockItemRegistrySupplier MOON_SAND = blockWithItem("moon_sand", ofFullCopy(Blocks.SAND));
     public static final BlockItemRegistrySupplier MOON_STONE = blockWithItem("moon_stone", ofFullCopy(Blocks.STONE));
+    public static final BlockItemRegistrySupplier MOON_STONE_IRON_ORE = blockWithItem("moon_stone_iron_ore", ofFullCopy(Blocks.STONE));
+
+    public static final BlockItemRegistrySupplier LUNAR_STONED_WOOD_LOG = blockWithItem("lunar_stoned_wood_log", ofFullCopy(Blocks.STONE), RotatedPillarBlock::new);
 
     public static final BlockItemRegistrySupplier ICED_MAGMA_BLOCK = blockWithItem("iced_magma_block", ofFullCopy(Blocks.MAGMA_BLOCK), IcedMagmaBlock::new);
 
