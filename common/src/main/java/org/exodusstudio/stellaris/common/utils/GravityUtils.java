e69d4e5a893c54eea68fe04311e17f9cf7ed85b6
Fej1Dev <101066376+Fej1Dev@users.noreply.github.com>
2025-11-29 00:06:59 +0200

    Add fall damage multiplier

--- common/src/main/java/org/exodusstudio/stellaris/common/utils/GravityUtils.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/utils/GravityUtils.java
@@ -17,11 +17,13 @@ import java.util.Map;
 
 public class GravityUtils {
 
-    public static final BigDecimal GRAVITY_CONVERSION_RATE = new BigDecimal("0.08").divide(new BigDecimal("9.81"), 20, RoundingMode.HALF_UP);
-    public static final BigDecimal SAFE_FALL_DISTANCE_CONVERSION_RATE = new BigDecimal("3").multiply(new BigDecimal("0.08"));
+    private static final BigDecimal EARTH_GRAVITY = new BigDecimal("9.81");
+    public static final BigDecimal GRAVITY_CONVERSION_RATE = new BigDecimal("0.08").divide(EARTH_GRAVITY, 20, RoundingMode.HALF_UP);
+    public static final BigDecimal SAFE_FALL_DISTANCE_CONVERSION_RATE = new BigDecimal("3").multiply(EARTH_GRAVITY);
 
     private static final Map<Planet, Double> GRAVITY_CACHE = new HashMap<>();
     private static final Map<Planet, Double> SAFE_FALL_DISTANCE_CACHE = new HashMap<>();
+    private static final Map<Planet, Double> FALL_DAMAGE_MULT_CACHE = new HashMap<>();
 
     public static void setGravity(LivingEntity entity) {
 
@@ -32,12 +34,7 @@ public class GravityUtils {
             return;
         }
 
-        double gravity = getGravity(planet);
-        double safeFallDistance = getSafeFallDistance(planet);
-
-        setEntityGravity(entity, gravity, safeFallDistance);
-
-        Stellaris.LOG.debug(String.valueOf(gravity));
+        setEntityGravity(entity, planet);
     }
 
     public static void trySetBaseAttribute(LivingEntity entity, Holder<Attribute> attribute, double value) {
@@ -48,14 +45,16 @@ public class GravityUtils {
 
     }
 
-    public static void setEntityGravity(LivingEntity entity, double gravity, double safeFallDistance) {
-        trySetBaseAttribute(entity, Attributes.GRAVITY, gravity);
-        trySetBaseAttribute(entity, Attributes.SAFE_FALL_DISTANCE, safeFallDistance);
+    public static void setEntityGravity(LivingEntity entity, Planet planet) {
+        trySetBaseAttribute(entity, Attributes.GRAVITY, getGravity(planet));
+        trySetBaseAttribute(entity, Attributes.SAFE_FALL_DISTANCE, getSafeFallDistance(planet));
+        trySetBaseAttribute(entity, Attributes.FALL_DAMAGE_MULTIPLIER, getFallDamageMult(planet));
     }
 
     public static void resetEntityGravity(LivingEntity entity) {
         trySetBaseAttribute(entity, Attributes.GRAVITY, Attributes.GRAVITY.value().getDefaultValue());
         trySetBaseAttribute(entity, Attributes.SAFE_FALL_DISTANCE, Attributes.SAFE_FALL_DISTANCE.value().getDefaultValue());
+        trySetBaseAttribute(entity, Attributes.FALL_DAMAGE_MULTIPLIER, Attributes.FALL_DAMAGE_MULTIPLIER.value().getDefaultValue());
     }
 
     public static double getGravity(@NotNull Planet planet) {
@@ -63,11 +62,21 @@ public class GravityUtils {
     }
 
     public static double getSafeFallDistance(Planet planet) {
-        return SAFE_FALL_DISTANCE_CACHE.computeIfAbsent(planet, p -> computeSafeFallDistance(getGravity(p)));
+        return SAFE_FALL_DISTANCE_CACHE.computeIfAbsent(planet, p -> computeSafeFallDistance(p.gravity()));
     }
 
-    public static double computeSafeFallDistance(double gravity) {
-        return SAFE_FALL_DISTANCE_CONVERSION_RATE.divide(new BigDecimal(gravity), 5, RoundingMode.HALF_UP).doubleValue();
+    /// @param newGravity in m/s²
+    public static double computeSafeFallDistance(String newGravity) {
+        return SAFE_FALL_DISTANCE_CONVERSION_RATE.divide(new BigDecimal(newGravity), 5, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    public static double getFallDamageMult(Planet planet) {
+        return FALL_DAMAGE_MULT_CACHE.computeIfAbsent(planet, p -> computeFallDamageMult(p.gravity()));
+    }
+
+    /// @param newGravity in m/s²
+    public static double computeFallDamageMult(String newGravity) {
+        return new BigDecimal(newGravity).divide(EARTH_GRAVITY, 5, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
