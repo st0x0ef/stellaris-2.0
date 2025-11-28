c5acfe487873e366b1233ba39c26a2900d9a3a59
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-11-28 16:20:41 -0500

    change gravity to string in codec

--- common/src/main/java/org/exodusstudio/stellaris/common/utils/GravityUtils.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/utils/GravityUtils.java
@@ -1,12 +1,10 @@
 package org.exodusstudio.stellaris.common.utils;
 
 import net.minecraft.core.Holder;
-import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import org.exodusstudio.stellaris.Stellaris;
 import org.exodusstudio.stellaris.common.data.Planet;
 import org.exodusstudio.stellaris.common.data.PlanetsData;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +17,8 @@ import java.util.Map;
 public class GravityUtils {
 
     public static final BigDecimal GRAVITY_CONVERSION_RATE = new BigDecimal("0.08").divide(new BigDecimal("9.81"), 20, RoundingMode.HALF_UP);
+
+
     private static final Map<Planet, Double> GRAVITY_CACHE = new HashMap<>();
 
     public static void setGravity(LivingEntity entity) {
@@ -29,8 +29,6 @@ public class GravityUtils {
             return;
 
         double gravity = getGravity(planet);
-
-        Stellaris.LOG.debug(String.valueOf(gravity));
 
         trySetBaseAttribute(entity, Attributes.GRAVITY, gravity);
         trySetBaseAttribute(entity, Attributes.SAFE_FALL_DISTANCE, 0.08 / gravity * 3);
@@ -53,9 +51,7 @@ public class GravityUtils {
      * @param MPS2 m/s²
      * @return Minecraft Gravity Unit (blocks/t²)
      */
-    //TODO replace double with string
-    public static double MPS2ToMCG(double MPS2) {
-        // replace with GRAVITY_CONVERSION_RATE.multiply(new BigDecimal(MPS2)).setScale(5, RoundingMode.HALF_UP).doubleValue();
-        return GRAVITY_CONVERSION_RATE.multiply(BigDecimal.valueOf(MPS2)).setScale(5, RoundingMode.HALF_UP).doubleValue();
+    public static double MPS2ToMCG(String MPS2) {
+        return GRAVITY_CONVERSION_RATE.multiply(new BigDecimal(MPS2)).setScale(5, RoundingMode.HALF_UP).doubleValue();
     }
 }
