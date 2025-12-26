b777e0c6dbac309817b2b18c8ec40ec437e3709b
TATHAN <costa.tathan@gmail.com>
2025-12-26 12:04:29 +0100

    Added rocket station texture/model

--- common/src/main/java/org/exodusstudio/stellaris/common/blocks/RocketStationBlock.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/blocks/RocketStationBlock.java
@@ -4,24 +4,32 @@ import com.mojang.serialization.MapCodec;
 import dev.architectury.registry.menu.ExtendedMenuProvider;
 import dev.architectury.registry.menu.MenuRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.exodusstudio.stellaris.common.menus.UpgradeStationMenu;
 import org.jetbrains.annotations.Nullable;
 
 public class RocketStationBlock extends Block {
+    public static final EnumProperty<Direction> FACING = BlockStateProperties.FACING;
 
     public static final MapCodec<RocketStationBlock> CODEC = simpleCodec(RocketStationBlock::new);
     private static final Component CONTAINER_TITLE = Component.translatable("container.stellaris.rocket_station");
@@ -30,6 +38,8 @@ public class RocketStationBlock extends Block {
 
     public RocketStationBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+
     }
 
     @Override
@@ -62,6 +72,26 @@ public class RocketStationBlock extends Block {
             MenuRegistry.openExtendedMenu((ServerPlayer) player, this.getMenuProvider(state, level, pos));
             return InteractionResult.CONSUME;
         }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getClockWise());
+    }
+
+    @Override
+    public BlockState rotate(BlockState blockState, Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
 }
