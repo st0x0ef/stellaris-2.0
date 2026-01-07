6411ef08b68b5d7860ffca18e4a6351328c636fe
st0x0ef <gabriel.gaudreault3@gmail.com>
2026-01-07 14:30:51 -0500

    update oxygen propagator texture

--- common/src/main/java/org/exodusstudio/stellaris/common/blocks/entities/machines/OxygenPropagatorBlockEntity.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/blocks/entities/machines/OxygenPropagatorBlockEntity.java
@@ -3,6 +3,9 @@ package org.exodusstudio.stellaris.common.blocks.entities.machines;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.exodusstudio.stellaris.Stellaris;
+import org.exodusstudio.stellaris.common.blocks.CoalGeneratorBlock;
+import org.exodusstudio.stellaris.common.blocks.OxygenPropagatorBlock;
 import org.exodusstudio.stellaris.common.blocks.base.BaseLitMachineBlock;
 import org.exodusstudio.stellaris.common.blocks.entities.machines.base.BaseEnergyBlockEntity;
 import org.exodusstudio.stellaris.common.registries.BlockEntitiesRegistry;
@@ -14,10 +17,14 @@ public class OxygenPropagatorBlockEntity extends BaseEnergyBlockEntity {
 
     @Override
     public void tick(Level level, BlockState state) {
-        if (energyContainer.getEnergy() > 0) {
-            state.setValue(BaseLitMachineBlock.LIT, true);
-        } else {
-            state.setValue(BaseLitMachineBlock.LIT, false);
+        boolean wasLit = state.getValue(OxygenPropagatorBlock.LIT);
+
+        if (energyContainer.getEnergy() > 0 && !wasLit) {
+            BlockState newState = state.setValue(OxygenPropagatorBlock.LIT, true);
+            level.setBlock(getBlockPos(), newState, 3);
+        } else if (energyContainer.getEnergy() == 0 && wasLit) {
+            BlockState newState = state.setValue(OxygenPropagatorBlock.LIT, false);
+            level.setBlock(getBlockPos(), newState, 3);
         }
     }
 }
