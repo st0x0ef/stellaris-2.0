713093aefd0d36d1428857df5e3a1340606b94df
TATHAN <costa.tathan@gmail.com>
2026-04-15 12:26:31 +0200

    Small tweaks

--- common/src/main/java/org/exodusstudio/stellaris/common/network/packets/TeleportToPlanetPacket.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/network/packets/TeleportToPlanetPacket.java
@@ -10,6 +10,8 @@ import org.exodusstudio.stellaris.common.utils.IdentifierUtils;
 import org.exodusstudio.stellaris.common.utils.TeleportUtil;
 import org.exodusstudio.stellaris.common.utils.Utils;
 
+import java.util.concurrent.CompletableFuture;
+
 public record TeleportToPlanetPacket(Planet destination) implements CustomPacketPayload {
 
     public static final CustomPacketPayload.Type<TeleportToPlanetPacket> TYPE = new CustomPacketPayload.Type<>(IdentifierUtils.id("teleport_to_planet"));
@@ -22,8 +24,8 @@ public record TeleportToPlanetPacket(Planet destination) implements CustomPacket
     public static void handle(TeleportToPlanetPacket data, NetworkManager.PacketContext context) {
         TeleportUtil.teleportToPlanet(context.getPlayer(), data.destination());
         context.getPlayer().stellaris$setPlanetMenuOpen(false, context.getPlayer(), true);
-        context.getPlayer().closeContainer();
         Utils.stopFade(context.getPlayer());
+        context.getPlayer().closeContainer();
 
     }
 
