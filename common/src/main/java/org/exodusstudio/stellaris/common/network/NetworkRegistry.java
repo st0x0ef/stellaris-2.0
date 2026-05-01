ed572dc065acc488f92c33e4c0003799d42446ba
TATHAN <costa.tathan@gmail.com>
2026-05-01 22:04:22 +0200

    Added titanium, silicon and fluid_cell wiki entry

--- common/src/main/java/org/exodusstudio/stellaris/common/network/NetworkRegistry.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/network/NetworkRegistry.java
@@ -24,7 +24,7 @@ public interface NetworkRegistry {
         registerC2S(KeyHandlerPacket.TYPE, KeyHandlerPacket.STREAM_CODEC, KeyHandlerPacket::handle);
         registerS2C(SyncOilLevelPacket.TYPE, SyncOilLevelPacket.STREAM_CODEC, SyncOilLevelPacket::handle);
         registerS2C(StartFadePacket.TYPE, StartFadePacket.STREAM_CODEC, StartFadePacket::handle);
-        registerS2C(OpenWikiEntry.TYPE, OpenWikiEntry.STREAM_CODEC, OpenWikiEntry::handle);
+        registerC2S(OpenWikiEntry.TYPE, OpenWikiEntry.STREAM_CODEC, OpenWikiEntry::handle);
 
         registerC2S(OpenRocketMenuPacket.TYPE, OpenRocketMenuPacket.STREAM_CODEC, OpenRocketMenuPacket::handle);
 
--- common/src/main/java/org/exodusstudio/stellaris/common/network/packets/OpenWikiEntry.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/network/packets/OpenWikiEntry.java
@@ -1,14 +1,17 @@
 package org.exodusstudio.stellaris.common.network.packets;
 
 import dev.architectury.networking.NetworkManager;
+import dev.architectury.registry.menu.MenuRegistry;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerPlayer;
 import org.exodusstudio.stellaris.Stellaris;
 import org.exodusstudio.stellaris.common.data.wiki.WikiPacks;
 import org.exodusstudio.stellaris.client.screens.tablet.application.wiki.WikiApplicationScreen;
+import org.exodusstudio.stellaris.common.menus.WikiApplicationMenu;
 
 public record OpenWikiEntry(Identifier entryId) implements CustomPacketPayload {
 
@@ -20,16 +23,10 @@ public record OpenWikiEntry(Identifier entryId) implements CustomPacketPayload {
     );
 
     public static void handle(OpenWikiEntry packet, NetworkManager.PacketContext context) {
-        NetworkManager.sendToServer(new OpenMenuPacket("wiki"));
-        Stellaris.LOG.error("" + context.getPlayer().containerMenu);
-
-        if(Minecraft.getInstance().screen instanceof WikiApplicationScreen wiki) {
-            var entryInfo = WikiPacks.ENTRY_COMPONENTS.get(packet.entryId);
-            if(entryInfo != null) {
-                wiki.setEntryInfo(entryInfo);
-
-            }
+        if(context.getPlayer() instanceof ServerPlayer serverPlayer) {
+            MenuRegistry.openExtendedMenu(serverPlayer, WikiApplicationMenu.createProvider(packet.entryId()));
         }
+
 
 
     }
