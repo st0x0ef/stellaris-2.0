e367c06cd99c291a50211c02d9e848c5bf888756
Ethan COSTA <costa.tathan@gmail.com>
2026-05-22 14:05:48 +0200

    Added rocket launched trigger

--- common/src/main/java/org/exodusstudio/stellaris/common/registries/AdvancementTriggerRegistry.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/registries/AdvancementTriggerRegistry.java
@@ -5,6 +5,7 @@ import net.minecraft.advancements.CriterionTrigger;
 import net.minecraft.core.registries.Registries;
 import org.exodusstudio.stellaris.Stellaris;
 import org.exodusstudio.stellaris.common.advancements.ParasiteAttachedTrigger;
+import org.exodusstudio.stellaris.common.advancements.RocketLaunchedTrigger;
 
 import java.util.function.Supplier;
 
@@ -16,6 +17,8 @@ public class AdvancementTriggerRegistry {
     public static final Supplier<ParasiteAttachedTrigger> PARASITE_ATTACHED =
             TRIGGER_TYPES.register("parasite_attached", ParasiteAttachedTrigger::new);
 
+    public static final Supplier<RocketLaunchedTrigger> ROCKET_LAUNCHED =
+            TRIGGER_TYPES.register("rocket_launched", RocketLaunchedTrigger::new);
 
 
 }
