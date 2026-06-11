682672376d78d730a66ea40b958b108480edc6b9
st0x0ef <gabriel.gaudreault3@gmail.com>
2026-06-11 16:08:52 -0400

    fix rover rendering

--- common/src/main/java/org/exodusstudio/stellaris/client/renderers/entity/vehicle/rover/RoverItemRenderer.java
+++ common/src/main/java/org/exodusstudio/stellaris/client/renderers/entity/vehicle/rover/RoverItemRenderer.java
@@ -1,18 +1,12 @@
 package org.exodusstudio.stellaris.client.renderers.entity.vehicle.rover;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 import com.mojang.serialization.MapCodec;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.SubmitNodeCollector;
-import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.special.SpecialModelRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.exodusstudio.stellaris.common.utils.IdentifierUtils;
 import org.jetbrains.annotations.Nullable;
@@ -20,27 +14,24 @@ import org.joml.Vector3fc;
 
 import java.util.function.Consumer;
 
-public record RoverItemRenderer() implements SpecialModelRenderer<Void> {
+public record RoverItemRenderer(RoverModel model) implements SpecialModelRenderer<Void> {
 
     private static final Identifier TEXTURE = IdentifierUtils.texture("entity/vehicle/rover");
 
     @Override
     public void submit(@Nullable Void state, PoseStack poseStack, SubmitNodeCollector nodeCollector, int packedLight, int packedOverlay, boolean hasFoil, int outlineColor) {
         poseStack.pushPose();
-
         poseStack.translate(0.5D, 1.5D, 0.5D);
         poseStack.scale(-1.0F, -1.0F, 1.0F);
-
-        RoverModel model = new RoverModel(Minecraft.getInstance().getEntityModels().bakeLayer(RoverModel.LAYER_LOCATION));
-
-        RenderType renderType = RenderTypes.entityCutout(TEXTURE);
-        nodeCollector.submitModelPart(model.root(), poseStack, renderType, packedLight, OverlayTexture.NO_OVERLAY, null);
-
+        nodeCollector.submitModelPart(model.root(), poseStack, RenderTypes.entityCutout(TEXTURE), packedLight, OverlayTexture.NO_OVERLAY, null);
         poseStack.popPose();
     }
 
     @Override
     public void getExtents(Consumer<Vector3fc> output) {
+        PoseStack poseStack = new PoseStack();
+        poseStack.translate(0.0D, 1.5D, 0.0D);
+        model.root().getExtentsForGui(poseStack, output);
     }
 
     @Override
@@ -53,7 +44,9 @@ public record RoverItemRenderer() implements SpecialModelRenderer<Void> {
 
         @Override
         public SpecialModelRenderer<?> bake(BakingContext context) {
-            return new RoverItemRenderer();
+            return new RoverItemRenderer(
+                    new RoverModel(context.entityModelSet().bakeLayer(RoverModel.LAYER_LOCATION))
+            );
         }
 
         @Override
