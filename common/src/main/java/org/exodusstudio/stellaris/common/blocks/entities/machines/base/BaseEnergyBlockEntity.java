e93245b4fb3df686b88013a2923e709aa062e365
st0x0ef <gabriel.gaudreault3@gmail.com>
2026-07-19 17:37:58 -0400

    fix energy bar sync

--- common/src/main/java/org/exodusstudio/stellaris/common/blocks/entities/machines/base/BaseEnergyBlockEntity.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/blocks/entities/machines/base/BaseEnergyBlockEntity.java
@@ -4,6 +4,9 @@ import com.fej1fun.potentials.providers.EnergyProvider;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -61,6 +64,16 @@ public abstract class BaseEnergyBlockEntity extends BlockEntity implements Energ
     protected void saveAdditional(ValueOutput output) {
         super.saveAdditional(output);
         energyContainer.save(output, "base");
+    }
+
+    @Override
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+        return this.saveWithFullMetadata(registries);
     }
 
     @Override
--- common/src/main/java/org/exodusstudio/stellaris/common/blocks/entities/machines/base/BaseEnergyContainerBlockEntity.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/blocks/entities/machines/base/BaseEnergyContainerBlockEntity.java
@@ -5,7 +5,10 @@ import com.fej1fun.potentials.providers.EnergyProvider;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
@@ -97,6 +100,16 @@ public abstract class BaseEnergyContainerBlockEntity extends BaseContainerBlockE
         super.saveAdditional(output);
         output.putInt(ENERGY_TAG, getEnergy(null).getEnergy());
         ContainerHelper.saveAllItems(output, items);
+    }
+
+    @Override
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+        return this.saveWithFullMetadata(registries);
     }
 
     @Override
