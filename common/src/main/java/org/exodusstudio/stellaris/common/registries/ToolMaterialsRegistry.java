1242bf85504c38065e8102c11cea26bbc646ff27
st0x0ef <gabriel.gaudreault3@gmail.com>
2026-07-20 18:24:26 -0400

    adjust titanium tools specs

--- common/src/main/java/org/exodusstudio/stellaris/common/registries/ToolMaterialsRegistry.java
+++ common/src/main/java/org/exodusstudio/stellaris/common/registries/ToolMaterialsRegistry.java
@@ -3,5 +3,5 @@ package org.exodusstudio.stellaris.common.registries;
 import net.minecraft.world.item.ToolMaterial;
 
 public class ToolMaterialsRegistry {
-    public static ToolMaterial TITANIUM = new ToolMaterial(TagsRegistry.BlockTags.INCORRECT_FOR_TITANIUM_TOOL, 1200, 7.0F, 3.0F, 14, TagsRegistry.ItemTags.TITANIUM_INGOTS);
+    public static ToolMaterial TITANIUM = new ToolMaterial(TagsRegistry.BlockTags.INCORRECT_FOR_TITANIUM_TOOL, 1200, 6.0F, 2.0F, 14, TagsRegistry.ItemTags.TITANIUM_INGOTS);
 }
